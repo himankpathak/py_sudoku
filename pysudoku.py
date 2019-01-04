a11c28e571c6d03362051b73afb2fabac7e3e23f
Himank Pathak <himankpathak@gmail.com>
2019-01-04 23:15:16 +0530

    initial commit

---
+++ pysudoku.py
@@ -0,0 +1,11 @@
+
+
+if __name__ == "__main__":
+
+	print("""
+	Welcome to SUD0KU v0.1
+
+    Objective: Enter integer number in empty spaces such that each row, column, and nonet can contain each number (typically 1 to 9) exactly once.
+              Every sudoku puzzle has only one possible solution. There can be no ambiguity in Sudoku.
+
+    """)
